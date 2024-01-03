52abf20af237dd7751790b6432b1d8bf07b8bb55
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-01-02 20:37:04 -0600

    completed flatten function

---
+++ README.md
@@ -0,0 +1,4 @@
+this project relies on Pipenv to manage dependencies. you will need both pipenv and python installed on your machine
+
+To run this project, naviage to the repository and run:
+    pipenv run python main.py
--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-import pandas as pd 
+# import pandas as pd 
+import os
+
+LOAD_FILE_DIR = "./load-files"
 
 def main():
-    print("hello world")
+   
+    main_menu_opts = ["Edit Configuration", "Load File", "Analyze File"] 
+    # while menu_choice != str(len(main_menu_opts) + 1):
+        
+    # load_files = get_load_files(LOAD_FILE_DIR)
 
-main()
+    
+def get_load_files(path: str):
+    if os.path.isabs(path):
+      os.mkdir(path)
+
+    list(map(lambda x: x, os.listdir(path)))
+
+def generate_menu(opts: [str]):
+    
+    def format_opts(options, count):
+        if len(options) == 0:
+            return Lis 
+
+
+    # formated_opts = 
+
+
+def flatten(item):
+
+    def flatten_calc(item, lst: [any], result: [any]):
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+        
+        if len(lst) > 0:
+            return flatten_calc(lst.pop(0), lst, result)
+
+        return result       
+
+    if not len(item) > 0:
+       return item
+
+    return flatten_calc(item.pop(0), item, [])
